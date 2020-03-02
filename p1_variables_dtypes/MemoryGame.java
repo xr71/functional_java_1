7c98992654b4b639ca6386e5b954d66df6e6c3e1
Xu Ren <musicalxu@hotmail.com>
2020-03-02 11:58:23 +0800

    int types

---
+++ p1_variables_dtypes/MemoryGame.java
@@ -0,0 +1,12 @@
+class MemoryGame {
+    public static void main(String argv[]) {
+        int passengers = 0;
+
+        passengers += 5;
+        passengers -= 3;
+        passengers -= 1;
+        passengers += 5;
+
+        System.out.println(passengers);
+    }
+}
