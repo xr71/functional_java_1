0557e859cb0e7187bf73db588c953cd5cd7a51eb
Xu Ren <musicalxu@hotmail.com>
2020-03-02 12:23:28 +0800

    other dtypes

---
+++ OtherTypes.java
@@ -0,0 +1,21 @@
+class OtherTypes {
+    public static void main(String argv[]) {
+
+        boolean fact = true;
+        // boolean number = 17; // will fail
+
+        char me = 'I';
+        String text = "text";
+        double price = 23.75;
+        // long total = 100.1;
+        // int trunc = 100.2;
+
+        System.out.println(fact);
+        System.out.println(me);
+        // System.out.println(total);
+        
+        // System.out.println(trunc);
+        System.out.println(price);
+        System.out.println(text);
+    }
+}
