91e775b6a3a66854dda951defb13bd4ed951471e
Xu Ren <musicalxu@hotmail.com>
2020-03-09 23:45:39 +0800

    logical expressions and case switch

--- p2_conditional_flow/Decisions.java
+++ p2_conditional_flow/Decisions.java
@@ -6,5 +6,31 @@ class Decisions {
         if (isCold) {
             System.out.println("It's cold!");
         }
+
+        float temp = 30;
+
+        if (temp < 32) {
+            System.out.println("It's cold");
+        }
+
+        // logical expressions
+        // && || !
+
+        // case and switch
+        int monthNum = 1;
+
+        String stringMonth = "";
+
+        switch (monthNum) {
+            case 1:
+                stringMonth = "January";
+                break;
+        
+            default:
+                stringMonth = "Invalid Month Number";
+                break;
+        }
+
+        System.out.println(stringMonth);
     }
 }
