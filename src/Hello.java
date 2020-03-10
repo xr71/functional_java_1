ab5acb1526efd0eb5b172ae642e0d04511656eb8
Xu Ren <musicalxu@hotmail.com>
2020-03-10 11:05:41 +0800

    project intellij init

---
+++ src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+   public static void greet(String name) {
+       System.out.println("Hello " + name);
+   }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Hello gr = new Hello();
+
+        gr.greet("Xu");
+    }
+}
